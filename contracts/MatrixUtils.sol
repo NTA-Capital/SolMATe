e797795bf616360a29e25f518a7afdf79b8e014b
Naveen Arun <notatroll.troll@gmail.com>
2021-12-28 01:04:45 -0600

    Update license

--- contracts/MatrixUtils.sol
+++ contracts/MatrixUtils.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: BSD-4-Clause
 pragma solidity >=0.8.4;
 import "prb-math/contracts/PRBMathSD59x18.sol";
 import "./VectorUtils.sol";
